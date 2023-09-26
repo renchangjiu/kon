aeab035a008342fa332b7feba183ab6e33ab8d0c
yui <cc@kkon.cc>
2023-09-26 11:08:02 +0800

    commit

--- Controls/WindowsTitleBar.axaml.cs
+++ Controls/WindowsTitleBar.axaml.cs
@@ -8,6 +8,7 @@ using Avalonia.Markup.Xaml;
 using Avalonia.Media;
 using kon.Utils;
 using kon.Views;
+using NLog;
 
 namespace kon.Controls;
 
@@ -15,9 +16,9 @@ public partial class WindowsTitleBar : UserControl {
 
     public WindowsTitleBar() {
         InitializeComponent();
-        MinimizeButton.Click += MinimizeWindow;
-        CloseButton.Click += CloseWindow;
-        MaximizeButton.Click += MaximizeWindow;
+        MinimizeButton.PointerPressed += MinimizeWindow;
+        CloseButton.PointerPressed += CloseWindow;
+        MaximizeBtn.PointerPressed += MaximizeWindow;
         SubscribeToWindowState();
     }
 
@@ -42,7 +43,9 @@ public partial class WindowsTitleBar : UserControl {
 
 
     private void OnPointerPressed(object? sender, PointerPressedEventArgs e) {
-        UiUtils.GetMainWindow(this).BeginMoveDrag(e);
+        if (sender is DockPanel) {
+            UiUtils.GetMainWindow(this)?.BeginMoveDrag(e);
+        }
     }
 
     private async void SubscribeToWindowState() {
@@ -55,25 +58,13 @@ public partial class WindowsTitleBar : UserControl {
 
         hostWindow.GetObservable(Window.WindowStateProperty).Subscribe(s => {
             if (s != WindowState.Maximized) {
-                MaximizeIcon.Data =
-                    Geometry.Parse("M2048 2048v-2048h-2048v2048h2048zM1843 1843h-1638v-1638h1638v1638z");
-                hostWindow.Padding = new Thickness(0, 0, 0, 0);
-                MaximizeToolTip.Content = "Maximize";
+                MaximizeBtn.Source = CommonUtils.getBitmapFromAsset("/Assets/Icons/maximize.png");
+                MaximizeBtn.HoverSource = CommonUtils.getBitmapFromAsset("/Assets/Icons/maximize_hover.png");
             }
 
             if (s == WindowState.Maximized) {
-                MaximizeIcon.Data =
-                    Geometry.Parse(
-                        "M2048 1638h-410v410h-1638v-1638h410v-410h1638v1638zm-614-1024h-1229v1229h1229v-1229zm409-409h-1229v205h1024v1024h205v-1229z");
-                hostWindow.Padding = new Thickness(7, 7, 7, 7);
-                MaximizeToolTip.Content = "Restore Down";
-
-                // This should be a more universal approach in both cases, but I found it to be less reliable, when for example double-clicking the title bar.
-                /*hostWindow.Padding = new Thickness(
-                        hostWindow.OffScreenMargin.Left,
-                        hostWindow.OffScreenMargin.Top,
-                        hostWindow.OffScreenMargin.Right,
-                        hostWindow.OffScreenMargin.Bottom);*/
+                MaximizeBtn.Source = CommonUtils.getBitmapFromAsset("/Assets/Icons/maximize_cancel.png");
+                MaximizeBtn.HoverSource = CommonUtils.getBitmapFromAsset("/Assets/Icons/maximize_cancel_hover.png");
             }
         });
     }
--- Utils/UiUtils.cs
+++ Utils/UiUtils.cs
@@ -6,9 +6,9 @@ namespace kon.Utils;
 
 public static class UiUtils {
 
-    public static MainWindow GetMainWindow(Visual? visual) {
+    public static MainWindow? GetMainWindow(Visual? visual) {
         TopLevel? topLevel = TopLevel.GetTopLevel(visual);
-        return (topLevel as MainWindow)!;
+        return topLevel as MainWindow;
     }
 
 }
